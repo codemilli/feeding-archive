ccfa000b872c6352e748441cb02bdf6ae55c1f0d
U-Yeong Ju <uyeong21c@gmail.com>
2019-03-20 11:53:03 +0900

    Remove appkey

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import firebase from 'firebase/app';
 import * as serviceWorker from './serviceWorker';
+import config from './config';
 import App from './containers/App';
 import 'firebase/auth';
 import './style.scss';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDdeg2ga2gD508X3kzAxtZROIXc-bX-mL4",
-  authDomain: "feeding-archive.firebaseapp.com",
-  databaseURL: "https://feeding-archive.firebaseio.com",
-  projectId: "feeding-archive",
-  storageBucket: "feeding-archive.appspot.com",
-  messagingSenderId: "217801729531"
-});
-
+firebase.initializeApp(config);
 render(<App />, document.getElementById('root'));
-
 serviceWorker.register();
